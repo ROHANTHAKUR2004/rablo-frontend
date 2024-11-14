bf102a7e73744533b294742b8d24b9f563726472
rohan <rohanthakur89768@gmail.com>
2024-11-14 16:26:46 +0530

    completed

--- src/helper/axiosinstance.ts
+++ src/helper/axiosinstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:8000",
-    //baseURL: "https://rablo-backend.vercel.app",
-   baseURL: "https://rablo-backend-five.vercel.app",
+    baseURL: "https://rablo-backend.vercel.app",
+//   baseURL: "https://rablo-backend-five.vercel.app/?vercelToolbarCode=y_6A-fY_0qF7lwt",
 });
 export default axiosInstance;
